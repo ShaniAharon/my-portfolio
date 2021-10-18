0b22ec6b7bea4a9a1e6830c59eef2bcfacb2185c
shforeal <rivena14798@gmail.com>
2021-10-18 12:18:52 +0300

    added controller, and about me

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+'use strict';
 console.log('Starting up');
+
+var gProjs = [
+  {
+    id: 'sokoban',
+    name: 'Sokoban',
+    title: 'Better push those boxes',
+    desc: 'lorem ipsum lorem ipsum lorem ipsum',
+    url: 'projs/sokoban',
+    publishedAt: 1448693940000,
+    labels: ['Matrixes', 'keyboard events'],
+  },
+];
+
+function getProjs() {
+  return gProjs;
+}
---
+++ js/portfolio-controller.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function onInit() {
+  renderProjs();
+}
+
+function renderProjs() {
+  var projs = getProjs();
+  var strHtml = projs.map((proj) => {
+    return `
+        <div class="col-md-4 col-sm-6 portfolio-item">
+        <a
+          class="portfolio-link"
+          data-toggle="modal"
+          href="#portfolioModal1"
+        >
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img
+            class="img-fluid"
+            src="img/portfolio/01-thumbnail.jpg"
+            alt=""
+          />
+        </a>
+        <div class="portfolio-caption">
+          <h4>${proj.name}</h4>
+          <p class="text-muted">${proj.desc}</p>
+        </div>
+      </div>
+        `;
+  });
+  const elGallery = document.querySelector('.proj-gallery');
+  elGallery.innerHTML = strHtml.join('');
+}
